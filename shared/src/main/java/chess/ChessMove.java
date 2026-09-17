a780c7dcc3887a63a17af706ab81661edb9f1492
Matthew Hepworth <mwhep@byu.edu>
2026-09-16 19:04:14 -0600

    ChessMove hash and equals

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -42,4 +42,22 @@ public class ChessMove {
     public ChessPiece.PieceType getPromotionPiece() {
         return promotionPiece;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(startPosition);
+        result = 31 * result + Objects.hashCode(endPosition);
+        result = 31 * result + Objects.hashCode(promotionPiece);
+        return result;
+    }
 }
